3346d2353066b0661e475f4e5425049f3acb7491
chakkaravarthivgts <chakkaravarthi.k@vishgyana.com>
2025-06-02 12:44:49 +0530

    added comment in app.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import Home from "./pages/Home";
 
-
 export default function App() {
   return (
     <div className="App">
       <Home />
+      {/* this is comment for checking git rebase */}
     </div>
   );
 }
